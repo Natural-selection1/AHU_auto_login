31ebdd8897e96be6507f1d4804f829a312353c53
自然选择 <natural_selection_@outlook.com>
2024-10-01 13:55:05 +0800

    删去了auto_login.py中的无用项, 并且格式化

--- auto_login.py
+++ auto_login.py
@@ -1,36 +1,40 @@
 import os
 import sys
 import subprocess
+
 import configparser  # 用于读取ini文件
 from playwright import sync_api  # 用于自动化操作浏览器
-import re
+
 
 def get_current_username():
     return os.getlogin()
+
 
 # 判断是否为wifi链接
 def is_connected_via_wifi(self):
     try:
         # 执行 netsh wlan show interfaces 命令
-        output = subprocess.check_output("netsh wlan show interfaces", shell=True, text=True)
+        output = subprocess.check_output(
+            "netsh wlan show interfaces", shell=True, text=True
+        )
         # 检查是否有无线连接
         if "状态" or "State" in output and "已连接" or "connected" in output:
             return True
         else:
             return False
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError:
         return False
+
 
 # 获取网关地址
 def get_default_gateway():
-    result = subprocess.run(['ipconfig'], capture_output=True, text=True)
-    
+    result = subprocess.run(["ipconfig"], capture_output=True, text=True)
+
     for line in result.stdout.splitlines():
         if "默认网关" in line or "Default Gateway" in line:
-
             gateway = line.split()[-1]
             return gateway
-    
+
     return None
 
 
@@ -54,22 +58,25 @@ class funcDocker(object):
             chromium_path = os.path.join(sys._MEIPASS, "chrome-win/chrome.exe")
         else:
             # !: 这里的路径可能需要根据自己电脑的实际情况进行修改
-            username = os.environ['USERNAME']
-            chromium_path = r"C:\\Users\\"+str(get_current_username())+"\AppData\Local\ms-playwright\chromium-1134\chrome-win\chrome.exe"
+            chromium_path = (
+                r"C:\\Users\\"
+                + str(get_current_username())
+                + "\AppData\Local\ms-playwright\chromium-1134\chrome-win\chrome.exe"
+            )
         return chromium_path
 
     # 执行自动登录的主要逻辑
     def run_auto_login(self):
         with sync_api.sync_playwright() as p:
             browser = p.chromium.launch(
-                headless=False,# *: 若要调试，请将headless=False
+                headless=False,  # *: 若要调试，请将headless=False
                 executable_path=self.chromium_path,
             )
             page = browser.new_page()
 
             # !: 无线网络似乎要优先访问 http://172.26.0.1/
             # !: 但无线网和有线网最后还是要跳转到 http://172.16.253.3/
-            if(is_connected_via_wifi(self) == True):
+            if is_connected_via_wifi(self):
                 url = "http://" + str(get_default_gateway()) + "/"
                 page.goto(url)
                 page.wait_for_timeout(1000)
